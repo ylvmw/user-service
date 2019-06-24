3913008c3b8c557569593b0ad2ae8bdd9c44efc4
yngliu <yngliu@yngliu-a01.vmware.com>
2019-06-24 11:08:48 +0800

    Auto create schema and insert data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user_info (id, name, gender, email, phone)
+VALUES ('1', 'John Snow', 'male', 'johnsnow@clouddemo.com', '12311212121');
+
+INSERT INTO user_info (id, name, gender, email, phone)
+VALUES ('2', 'Arya Stark', 'female','aryastark@clouddemo.com', '12312123434');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,10 +6,3 @@ CREATE TABLE user_info (
   gender    TEXT,
   email     TEXT,
   phone     TEXT);
-
-
-INSERT INTO user_info (id, name, gender, email, phone)
-VALUES ('1', 'John Snow', 'male', 'johnsnow@clouddemo.com', '12311212121');
-
-INSERT INTO user_info (id, name, gender, email, phone)
-VALUES ('2', 'Arya Stark', 'female','aryastark@clouddemo.com', '12312123434');
